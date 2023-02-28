04a1adfff01d56af31fc35a5267f5bb76ecfd201
unknown <lalit4283@gmail.comgit config --global user.name Lalitsingh28>
2023-02-28 16:48:40 +0530

    Started

---
+++ BloggingAppBackend/src/main/java/com/example/demo/payload/UserDto.java
@@ -0,0 +1,17 @@
+package com.example.demo.payload;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDto {
+	
+	private Integer id;
+	private String name;
+	private String email;
+	private String password;
+
+}
