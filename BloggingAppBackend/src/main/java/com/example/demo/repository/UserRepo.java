aa1079921d32bbda2f3fe9bf3ddab8c2d2e8d010
unknown <lalit4283@gmail.com>
2023-04-21 13:59:28 +0530

    Spring Security

--- BloggingAppBackend/src/main/java/com/example/demo/repository/UserRepo.java
+++ BloggingAppBackend/src/main/java/com/example/demo/repository/UserRepo.java
@@ -1,6 +1,5 @@
 package com.example.demo.repository;
 
-import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -10,5 +9,5 @@ import com.example.demo.entity.User;
 @Repository
 public interface UserRepo extends JpaRepository<User, Integer>{
 
-	Optional<User> findByEmail(String email);
+	public User findByEmail(String email);
 }
