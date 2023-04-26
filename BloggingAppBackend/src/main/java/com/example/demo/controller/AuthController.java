37218fb997a910b98882a9841e965a273d00fa02
unknown <lalit4283@gmail.com>
2023-04-26 15:20:24 +0530

    Backend Completed

--- BloggingAppBackend/src/main/java/com/example/demo/controller/AuthController.java
+++ BloggingAppBackend/src/main/java/com/example/demo/controller/AuthController.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.demo.config.CustomUser;
 import com.example.demo.entity.User;
 import com.example.demo.payload.JwtAuthRequest;
+import com.example.demo.payload.JwtAuthResponse;
 import com.example.demo.payload.UserDTO;
 import com.example.demo.repository.UserRepo;
 import com.example.demo.security.JwtUtils;
@@ -42,26 +44,37 @@ public class AuthController {
 
 //	login
 	
-	@GetMapping("/login")
-    public String getToken(@RequestBody JwtAuthRequest authRequest) throws Exception {
+	@PostMapping("/login")
+    public ResponseEntity<JwtAuthResponse> getToken(@RequestBody JwtAuthRequest authRequest) throws Exception {
         // Get user details
         UserDetails userDetails = customUser.loadUserByUsername(authRequest.getUsername());
 
-        if(passwordEncoder.matches(authRequest.getPassword(), userDetails.getPassword())){
-            // Generate token
-            return jwtUtils.generateToken(authRequest.getUsername());
+        if(passwordEncoder.matches(authRequest.getPassword(), userDetails.getPassword())){  
+			try {
+				// Generate token
+				String token = jwtUtils.generateToken(authRequest.getUsername());
+				
+				// response
+	            JwtAuthResponse response = new JwtAuthResponse();
+	            response.setToken(token);
+	            response.setUser(mapper.map(userDetails, UserDTO.class));
+	            return new ResponseEntity<JwtAuthResponse>(response,HttpStatus.ACCEPTED);
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+            
         }
-
         throw new Exception("User details invalid.");
     }
 
 
-	// get loggedin user data
+	// get logged in user data
 
 	@GetMapping("/currentuser")
 	public ResponseEntity<UserDTO> getUser(Principal principal) {
 		User user = this.userRepo.findByEmail(principal.getName());
-		return new ResponseEntity<UserDTO>(this.mapper.map(user, UserDTO.class), HttpStatus.OK);
+		return new ResponseEntity<UserDTO>(mapper.map(user, UserDTO.class), HttpStatus.OK);
 	}
 
 }
